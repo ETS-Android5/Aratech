ee4d10f18c8b6222897359ff67c847d41d3165d2
Francis Gyimah <fgyimah883@gmail.com>
2020-02-08 11:48:53 +0000

    changed forum_id of message model to forum

--- server/models/Message.js
+++ server/models/Message.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // creating MessageSchema
 const MessageSchema = new Schema({
-  forum_id: {
+  forum: {
     type: Schema.Types.ObjectId,
     ref: 'Forum',
     required: true

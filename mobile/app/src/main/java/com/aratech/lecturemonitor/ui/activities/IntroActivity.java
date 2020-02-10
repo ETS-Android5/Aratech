30ff449d9304b5172347dcdd3c3e654ad8f38ea6
XxkayxX <60482112+XxkayxX@users.noreply.github.com>
2020-02-10 17:01:28 +0000

    restructured mobile app

--- mobile/app/src/main/java/com/aratech/lecturemonitor/ui/activities/IntroActivity.java
+++ mobile/app/src/main/java/com/aratech/lecturemonitor/ui/activities/IntroActivity.java
@@ -1,8 +1,10 @@
-package com.aratech.lecturemonitor;
+package com.aratech.lecturemonitor.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.aratech.lecturemonitor.R;
 
 public class IntroActivity extends AppCompatActivity {
 

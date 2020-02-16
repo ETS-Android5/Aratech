2bbfda46cc821d95fc074becdd3dac9225bb4d88
Donald Addei <asantedonald521@gmail.com>
2020-02-16 20:30:16 +0000

    created base forgot password and password reset pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import NavMobile from './components/NavMobile';
-import Landing from './pages/Landing';
-import StdSignUp from './pages/StdSignup';
-import StdSignIn from './pages/StdSignIn';
-import Footer from './components/Footer';
+import NavMobile from "./components/NavMobile";
+import Landing from "./pages/Landing";
+import StdSignUp from "./pages/StdSignup";
+import StdSignIn from "./pages/StdSignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Footer from "./components/Footer";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
   render() {
@@ -18,8 +20,11 @@ class App extends React.Component {
           <Route exact path="/about" component={Landing} />
           <Route exact path="/student/signup" component={StdSignUp} />
           <Route exact path="/student/signin" component={StdSignIn} />
+          <Route exact path="/student/signin" component={StdSignIn} />
+          <Route exact path="/forgotpassword" component={ForgotPassword} />
+          <Route exact path="/resetpassword" component={ResetPassword} />
         </Switch>
-        {/* Include the mobile nav in every page */}
+        {/* Include the mobile nav and footer in every page */}
         <NavMobile />
         <Footer />
       </Router>
---
+++ frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+import Navbar from "../components/Navbar";
+
+class ForgotPassword extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="uk-grid-collapse" data-uk-grid>
+          <div
+            className="uk-width-1-2@m uk-padding-large uk-flex uk-flex-middle uk-flex-center"
+            data-uk-height-viewport
+          >
+            <div className="uk-width-3-4@s">
+              <div className="uk-text-center uk-margin-medium-bottom">
+                <h3 className="uk-letter-spacing-small">
+                  Enter IndexNo / Email to reset your password
+                </h3>
+              </div>
+
+              <form>
+                <div className="uk-width-1-1 uk-margin">
+                  <label className="uk-form-label" for="name">
+                    Index No / Email
+                  </label>
+                  <input
+                    id="idEmail"
+                    className="uk-input uk-form-large"
+                    type="text"
+                    placeholder="Index or Email"
+                  />
+                </div>
+                <div className="uk-width-1-1 uk-text-center">
+                  <button className="uk-button uk-button__animate uk-button-primary uk-button-large">
+                    Send password reset email
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div
+            className="uk-width-1-2@m uk-padding-large uk-flex uk-flex-middle uk-flex-center uk-light
+    uk-background-cover uk-background-norepeat uk-background-blend-overlay uk-background-primary"
+            data-uk-height-viewport
+          >
+            <div>
+              <div className="uk-text-center">
+                <h2 className="uk-h1 uk-letter-spacing-small">
+                  Hello There, Join Us and Lets monitor your lectures for you.
+                </h2>
+              </div>
+              <div className="uk-margin-top uk-margin-medium-bottom uk-text-center">
+                <p>Enter your personal details and join us</p>
+              </div>
+              <div className="uk-width-1-1 uk-text-center">
+                <a
+                  href="sign-up.html"
+                  className="uk-button uk-button-success-outline uk-button-large"
+                >
+                  Sign Up
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ForgotPassword;

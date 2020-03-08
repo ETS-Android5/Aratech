e432b89d91920a1c9ef54e61060ee5f989795e29
Francis Gyimah <fgyimah883@gmail.com>
2020-03-08 23:48:44 +0000

    removed sorting of departments

--- server/controllers/departmentController.js
+++ server/controllers/departmentController.js
@@ -1,7 +1,7 @@
 const Department = require('../models/Department');
 
 exports.getDepartments = async (req, res) => {
-  const departments = await Department.find().sort({ name: 1 });
+  const departments = await Department.find();
 
   res.status(200).json({
     status: 'success',

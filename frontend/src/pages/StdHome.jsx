6f334e74866e765e82cb35a70dce8f2841597c25
Francis Gyimah <fgyimah883@gmail.com>
2020-03-29 23:05:43 +0000

    added calendar component to the frontend

--- frontend/src/pages/StdHome.jsx
+++ frontend/src/pages/StdHome.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import UIKit from 'uikit';
-import ImageUploader from 'react-images-upload';
+import React from "react";
+import { connect } from "react-redux";
+import UIKit from "uikit";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import ImageUploader from "react-images-upload";
+
+import Navbar from "../components/Navbar";
+
+//use moment as defaullt localizer for calendar
+const localizer = momentLocalizer(moment);
 
 import { setStudentProfileImg } from "../store/actions/authActions";
 
@@ -10,14 +17,14 @@ class Home extends React.Component {
     super(props);
 
     this.state = {
-      image: ''
+      image: ""
     };
   }
 
   componentDidMount() {
     //verify if user has a profile image, if not set one
     if (!this.props.user.avatar) {
-      UIKit.modal('#set-avatar', {
+      UIKit.modal("#set-avatar", {
         bgClose: false,
         escClose: false,
         modal: false,
@@ -32,10 +39,10 @@ class Home extends React.Component {
     });
   };
 
-  uploadImage = () => {
-    const isSet = this.props.setStudentProfileImg(this.state.image);
-    if(isSet) {
-      UIKit.modal('#set-avatar').hide();
+  uploadImage = async () => {
+    const isSet = await this.props.setStudentProfileImg(this.state.image);
+    if (isSet) {
+      UIKit.modal("#set-avatar").hide();
     } else {
       //todo: decide what to do later
     }
@@ -44,6 +51,19 @@ class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <Navbar />
+        {/**calendar component */}
+        <Calendar
+          localizer={localizer}
+          events={[]}
+          startAccessor="startTime"
+          endAccessor="endTime"
+          views={["day", "week", "agenda"]}
+          defaultView={"day"}
+          style={{ height: 50 }}
+        />
+
+        {/** modal to set user profile picture */}
         <div id="set-avatar" data-uk-modal>
           <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
             <h2 className="uk-modal-title">SET YOUR PROFILE PICTURE</h2>
@@ -52,7 +72,7 @@ class Home extends React.Component {
               withIcon={true}
               buttonText="Choose image"
               onChange={this.onFileChange}
-              imgExtension={['.jpg', '.png']}
+              imgExtension={[".jpg", ".png"]}
               withPreview={true}
               label="Max file size is 5mb. 
               Accepted image types are .png and .jpg"
@@ -76,4 +96,4 @@ const mapStateToProps = ({ auth: { user } }) => ({
   user
 });
 
-export default connect(mapStateToProps,{setStudentProfileImg})(Home);
+export default connect(mapStateToProps, { setStudentProfileImg })(Home);

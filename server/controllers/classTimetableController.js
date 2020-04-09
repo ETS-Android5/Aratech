e8a6d264b104ea0b9714d57c8b97098f80f70102
Godfred Doe <godfredoe9@gmail.com>
2020-04-09 21:18:44 +0000

    created class timetable controller to setup, get and delete timetable contents

--- server/controllers/classTimetableController.js
+++ server/controllers/classTimetableController.js
@@ -1,13 +1,99 @@
 const Joi = require('@hapi/joi');
 const jwt = require('jsonwebtoken');
 
-const personalTimetable = require('../models/PersonalTimetable');
-const classTimetable = require('../models/ClassTimetable');
+const ClassTimetable = require('../models/ClassTimetable');
+const Event = require('../models/Event');
 
-// student personal timetable
-exports.personalTimetableSetup = async (req, res) => {
-  const schema = Joi.object({});
+// setting up class timetable. this is done by the course rep
+exports.addEventToClassTimetable = async (req, res) => {
+  const student = req.user.student;
+
+  if (!student) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Must be logged in as student to add to class time table',
+    });
+  }
+  // check if the user is the class rep
+  if (!student.cRep) {
+    return res.status(403).json({
+      status: 'fail',
+      message:
+        'Unauthorized, must be a classrep to add an event to class time table',
+    });
+  }
+
+  //validate user data
+  const schema = Joi.object({
+    programme: Joi.string().required(),
+    eventName: Joi.string().required(),
+    startTime: Joi.date().required(),
+    endTime: Joi.string().required(),
+    repeatDaily: Joi.boolean().optional(),
+    repeatWeekly: Joi.boolean().optional(),
+  });
+  try {
+    await schema.validateAsync(req.body);
+  } catch (error) {
+    return res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+
+  const event = await Event.create(req.body);
+
+  //check if class does not already have a time table setup
+  const cTable = await Classimetable.findOne({ programme: student.programme });
+  if (cTable) {
+    //class time table already exists, push the new event and save it
+    cTable.events.push({ eventId: event._id });
+    await cTable.save();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        classTimetable: cTable,
+      },
+    });
+  } else {
+    //create a new class timetable and add the event
+    const newCT = await ClassTimetable.create({
+      programme: student.programme,
+      events: [{ eventId: event._id }],
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        classTimeTable: newCT,
+      },
+    });
+  }
 };
 
-//class timetable
-exports.classTimetableSetup = async (req, res) => {};
+exports.getClassTimetable = async (req, res) => {
+  //check if user is student
+  const student = req.user.student;
+
+  if (!student) {
+    return res.status(400).json({
+      status: 'fail',
+      message:
+        'Must be logged in as a student to view your personal time table',
+    });
+  }
+
+  //get user programme
+  const programme = student.programme;
+
+  //get user class time table
+  const cTable = await TimeTable.findOne({
+    programme: student.programme,
+  }).populate('eventId');
+  res.status(200).json({
+    status: 'success',
+    data: {
+      classTimeTable: cTable ? cTable : {},
+    },
+  });
+};

76dddf921f74674201425a92a6171083e3f265b1
Gyimah Francis <fgyimah883@gmail.com>
2020-04-25 18:57:08 +0000

    removed unwanted git tags

--- mobile/app/src/main/java/com/aratech/lecturemonitor/ui/activities/lecturers/LecturerHomeActivity.java
+++ mobile/app/src/main/java/com/aratech/lecturemonitor/ui/activities/lecturers/LecturerHomeActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.aratech.lecturemonitor.ui.activities.lecturers;
 
 import androidx.annotation.NonNull;
@@ -102,21 +101,3 @@ public class LecturerHomeActivity extends AppCompatActivity  implements Navigati
         Tools.setSystemBarColor(this, R.color.black);
     }
 }
-=======
-package com.aratech.lecturemonitor.ui.activities.lecturers;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import com.aratech.lecturemonitor.R;
-
-public class LecturerHomeActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lecturer_home);
-    }
-}
->>>>>>> master

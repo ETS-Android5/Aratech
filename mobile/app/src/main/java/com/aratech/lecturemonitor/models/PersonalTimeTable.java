4ff8d094cff5594b5005eec83cc431b6601fc84d
emmanuel7894561230 <emmanueltweneboah71@gmail.com>
2020-06-22 15:39:49 +0000

    created personal time table model for mobile

---
+++ mobile/app/src/main/java/com/aratech/lecturemonitor/models/PersonalTimeTable.java
@@ -0,0 +1,41 @@
+package com.aratech.lecturemonitor.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class PersonalTimeTable {
+    
+    @SerializedName('userId')
+    @Expose
+    private String userId;
+
+    @SerializedName('events')
+    @Expose
+    private List<Event> events;
+
+    public PersonalTimeTable() {}
+
+    public PersonalTimeTable(String userId, List<Event> events) {
+        this.userId = userId;
+        this.events = events;
+    }
+
+    private void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    private String getUserId() {
+        return userId;
+    }
+
+    private void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    private List<Events> getEvents() {
+        return events;
+    }
+
+}
